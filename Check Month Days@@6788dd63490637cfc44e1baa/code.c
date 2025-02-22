6b4f20ff007f8494b65f8a2bb3a515fba845bda6
sakshi <sakshiosho540@gmail.com>
2025-02-22 22:06:47 +0530

    Solution to Check Month Days

---
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    int a,b;
+    scanf("%d",&a);
+    if (a==1 || a==3 || a==5 || a==7 || a==8 ||a==10|| a==12){
+        printf("31");
+    }
+    else if{ a==2}{
+        printf("28");
+        
+    }
+    else{printf("30")}
+}
