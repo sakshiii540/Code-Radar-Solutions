daf1dd7e02dac70e14a3b85e13309c0ddaed5885
sakshi <sakshiosho540@gmail.com>
2025-02-27 20:38:27 +0530

    Solution to Print Floyd's Triangle

--- Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
+++ Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
@@ -1,13 +1,13 @@
 // Your code here...
 #include <stdio.h>
 int main(){
-    int i,j,k,n;
+    int i,j,count=0,n;
     scanf("%d",&n);
     for(i=1;i<=n;i++){
-        for(j=1;j<=i;j++){
-            k=j;
-            k=k+1;}
-            printf("%d ",k);
+        for(j=i+count;j<=count+1;j++){
+            count++;
+            }
+            printf("%d ",j);
             
         
         printf("\n");
