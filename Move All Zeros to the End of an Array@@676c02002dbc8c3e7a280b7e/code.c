b0ca1c97f82e23cd1b6a4f56cca815cd459273d1
sakshi <sakshiosho540@gmail.com>
2025-04-07 18:35:44 +0530

    Solution to Move All Zeros to the End of an Array

---
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -0,0 +1,21 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    int i,j,k,l=0,n;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+     for (i=0;i<n;i++){
+        if (arr[i]!=0){
+            printf("%d ",arr[i]);
+        }
+        else{
+            l++;
+            for (i=0;i<l;i++){
+                printf("0 ");
+            }
+        }
+     }
+}
