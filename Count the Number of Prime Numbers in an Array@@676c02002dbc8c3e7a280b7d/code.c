990ecdb24722d7b142b8aa2d6a955d62eef8c7bb
sakshi <sakshiosho540@gmail.com>
2025-04-08 08:56:00 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -1,13 +1,14 @@
 // Your code here...
 #include <stdio.h>
 int main(){
-    int n,i,j,k=0;
+    int n,i,j,k;
     scanf("%d",&n);
     int arr[n];
     for (i=0;i<n;i++){
         scanf("%d",&arr[i]);
 
     }
+    k=0;
     for (i=0;i<n;i++){
         for (j=1;j<=arr[i];j++){
         if (arr[i]%j==0){
