9848b1c79ad4afc7259580e0641ba050961fd0f5
sakshi <sakshiosho540@gmail.com>
2025-04-09 17:12:13 +0530

    Solution to Count the Number of Palindromic Numbers in an Array

---
+++ Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
@@ -0,0 +1,28 @@
+// Your code here...
+// Your code here...
+// Your code here...
+#include <stdio.h>
+int main(){
+    int i,j,k,x,n,m=0,l=0;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for (i=0;i<n-1;i++){
+        if (arr[i+1]==arr[i]+1 ){
+            l++;
+        }
+        if (arr[i+1]==arr[i]-1 ){
+            m++;
+        }
+    }
+    // printf("%d\n",l);
+    //  printf("%d",m);
+    if (m==l){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
